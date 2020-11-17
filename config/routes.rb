379b5049ec49e1236bd1a1ce6fff32c1c6f5e2b9
Sami Halim <samihalim@gmail.com>
2020-11-17 18:36:35 +0100

    working articles dummy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+ #root to: 'articles#index'
+ #get 'articles', to: 'articles#index'
+ #post 'articles', to: 'articles#create'
+ #get 'articles/new', to: 'articles#new', as: :new_article
+ #get 'articles/:id', to: 'articles#show', as: :article
+ #get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+ #patch 'articles/:id', to: 'articles#update'
+ #delete 'articles/:id', to: 'articles#destroy'
+
 end
+
